cb081aa37676b5a7da9cdee4bc368951f03ac6f0
김주연 <kirimaki1@nate.com>
2023-02-03 11:21:07 +0900

    node_module git ignore

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+app.use(express.urlencoded({extended: true}));
 
-app.listen(9092, function() {
-    console.log('listening on 9092');
-});
-
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/index.html')
-});
-
-app.get('/write', (req, res) => {
-    res.sendFile(`${__dirname}/write.html`);
-});
-
-app.get('/test', (req, res) => {
-    res.send('여긴 테스트다');
+let db;
+MongoClient.connect('mongodb+srv://admin:1q2w3e4r@cluster0.v4wxlfs.mongodb.net/?retryWrites=true&w=majority', (err, client) => {
+    if(err) return console.log(err);
+    db = client.db('todoApp');
+    
+    app.listen(8080, function() {
+        console.log('connected success');
+    });
 });
