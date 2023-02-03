8eb69957546e9f5a5c7177e3b06c380f2f15a2f4
김주연 <kirimaki1@nate.com>
2023-02-03 18:07:56 +0900

    node study

--- server.js
+++ server.js
@@ -8,7 +8,24 @@ MongoClient.connect('mongodb+srv://admin:1q2w3e4r@cluster0.v4wxlfs.mongodb.net/?
     if(err) return console.log(err);
     db = client.db('todoApp');
     
-    app.listen(8080, function() {
+    app.listen(8081, function() {
         console.log('connected success');
     });
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+})
+
+app.get('/list', (req, res) => {
+    db.collection('post').find().toArray((err, result) => {
+        res.sendFile(`${__dirname}/list.html`);
+        console.log(err);
+        console.log(result);
+    });
+})
+
+app.delete('/delete', (req, res) => {
+    res.send('야 잘받앗다');
+    console.log(req.body);
+})
